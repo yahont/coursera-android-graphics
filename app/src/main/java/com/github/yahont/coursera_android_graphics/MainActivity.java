f523e7922e59cfba35e064a324bc2a0d61df2ba6
Iakov S <yahont@github.com>
2023-01-28 23:08:18 +0000

    first opengl sample program

--- app/src/main/java/com/github/yahont/coursera_android_graphics/MainActivity.java
+++ app/src/main/java/com/github/yahont/coursera_android_graphics/MainActivity.java
@@ -1,18 +1,29 @@
 package com.github.yahont.coursera_android_graphics;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MyView mMyView=null;//a custom view for drawing
+    private MyView GLView;
+    private View mControlsView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();//hide the title bar
-        mMyView=new MyView(this);
-        setContentView(mMyView);
+        GLView =new MyView(this);
+        //setContentView(R.layout.activity_fullscreen);
+        setContentView(GLView);
+        //set full screen
+        mControlsView=getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        mControlsView.setSystemUiVisibility(uiOptions);
+
     }
 }
